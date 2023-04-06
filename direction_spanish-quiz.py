5265d8450f82c92a7fc0cd8f806b7f3b8392377f
MADD467 <129204408+MADD467@users.noreply.github.com>
2023-04-06 13:22:58 +0200

    Add files via upload

---
+++ direction_spanish-quiz.py
@@ -0,0 +1,116 @@
+
+
+score = 0
+
+for i in range(1):
+    pregunta1 = "Como se dice tout droit ?"
+    respuesta1 = "todo recto o seguir todo recto o todo derecho o continuar"
+    print(pregunta1)
+    respuesta11 = input("Respuesta: ")
+
+    if respuesta1 == respuesta11:
+        print("Correcto")
+        score = score + 1
+        print(score)
+    else:
+        print("Incorrecto")
+        score = score - 1
+        print(score)
+
+    pregunta2 = "Como se dice tourner à droite ?"
+    respuesta2 = "girar a la derecha"
+    print(pregunta2)
+    respuesta22 = input("Respuesta: ")
+
+    if respuesta2 == respuesta22:
+        print("Correcto")
+        score = score + 1
+        print(score)
+    else:
+        print("Incorrecto")
+        score = score - 1
+        print(score)
+
+    pregunta3 = "Como se dice tourner à gauche ?"
+    respuesta3 = "girar a la izquierda"
+    print(pregunta3)
+    respuesta33 = input("Respuesta: ")
+
+    if respuesta3 == respuesta33:
+        print("Correcto")
+        score = score + 1
+        print(score)
+    else:
+        print("Incorrecto")
+        score = score - 1
+        print(score)
+
+    pregunta4 = "Como se dice passer a coté  ?"
+    respuesta4 = "pasar al lado de ..."
+    print(pregunta4)
+    respuesta44 = input("Respuesta: ")
+
+    if respuesta4 == respuesta44:
+        print("Correcto")
+        score = score + 1
+        print(score)
+    else:
+        print("Incorrecto")
+        score = score - 1
+        print(score)
+
+    pregunta5 = "Como se dice tourner traverser ?"
+    respuesta5 = "cruzar o atravesar"
+    print(pregunta5)
+    respuesta55 = input("Respuesta: ")
+
+    if respuesta5 == respuesta55:
+        print("Correcto")
+        score = score + 1
+        print(score)
+    else:
+        print("Incorrecto")
+        score = score - 1
+        print(score)
+
+    pregunta6 = "Como se dice monter ?"
+    respuesta6 = "subir"
+    print(pregunta6)
+    respuesta66 = input("Respuesta: ")
+
+    if respuesta6 == respuesta6:
+        print("Correcto")
+        score = score + 1
+        print(score)
+    else:
+        print("Incorrecto")
+        score = score - 1
+        print(score)
+
+    pregunta7 = "Como se dice descendre ?"
+    respuesta7 = "bajar"
+    print(pregunta7)
+    respuesta77 = input("Respuesta: ")
+
+    if respuesta7 == respuesta77:
+        print("Correcto")
+        score = score + 1
+        print(score)
+    else:
+        print("Incorrecto")
+        score = score - 1
+        print(score)
+
+    pregunta8 = "Como se dice aller à ?"
+    respuesta8 = "Ir hacia..."
+    print(pregunta8)
+    respuesta88 = input("Respuesta: ")
+
+    if respuesta8 == respuesta88:
+        print("Correcto")
+        score = score + 1
+        print(score)
+    else:
+        print("Incorrecto")
+        score = score - 1
+        print(score)
